088142998677270da51d963239fa5b0d7380931d
Hancie123 <hanciewanemphago@gmail.com>
2024-11-28 22:50:40 +0545

    login added

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,34 +3,61 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\LoginRequest;
 use App\Http\Requests\User\UserCreateRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
-    public function signup(UserCreateRequest $request){
-        try{
-            
-            $user=DB::transaction(function()use($request){
-                $user=User::create([
-                    'name'=>$request->name,
-                    'email'=>$request->email,
-                    'phone'=>$request->phone,
-                    'password'=>$request->password,
-                    'location'=>$request->location,
-                    'address'=>$request->address
+
+    public function login(LoginRequest $request)
+    {
+
+        $email = $request->email;
+        $password = $request->password;
+
+        try {
+            if (Auth::attempt(['email' => $email, 'password' => $password])) {
+                $user = Auth()->user();
+                $token = $user->createToken('login_token')->accessToken;
+                $data = [
+                    'user' => new UserResource($user),
+                    'token' => $token,
+
+                ];
+                return responseSuccess($data, 200, 'Login success!');
+            } else {
+                return responseError('Invalid email or password!', 500);
+            }
+        } catch (\Throwable $th) {
+            return responseError($th->getMessage(), 500);
+        }
+    }
+
+    public function signup(UserCreateRequest $request)
+    {
+        try {
+
+            $user = DB::transaction(function () use ($request) {
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'phone' => $request->phone,
+                    'password' => $request->password,
+                    'location' => $request->location,
+                    'address' => $request->address
                 ]);
                 return $user;
             });
-            if($user){
-                return responseSuccess(new UserResource($user),200,'User Account Created Successfully!');
+            if ($user) {
+                return responseSuccess(new UserResource($user), 200, 'User Account Created Successfully!');
             }
-        }
-        catch(\Exception $e){
-            return responseError($e->getMessage(),500);
+        } catch (\Exception $e) {
+            return responseError($e->getMessage(), 500);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,6 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::post('login', [AuthController::class, 'login']);
 Route::post('create-account', [AuthController::class, 'signup']);
