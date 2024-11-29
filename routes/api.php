3383eb2faa7c91f9965ee88e24294729012c9491
Hancie123 <hanciewanemphago@gmail.com>
2024-11-29 14:12:09 +0545

    vendor route updated

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('create-account', [AuthController::class, 'signup']);
 Route::get('category', [CategoryController::class, 'category']);
+Route::post('vendor-signup', [VendorController::class, 'store']);
 
 Route::middleware(['auth:api'])->group(function () {
-    Route::post('vendor-signup', [VendorController::class, 'store']);
+    
 });
