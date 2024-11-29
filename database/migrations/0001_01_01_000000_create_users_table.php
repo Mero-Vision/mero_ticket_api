0970ed50271e5cb451c80ed6933b587f2d2a2285
Hancie123 <hanciewanemphago@gmail.com>
2024-11-29 16:32:48 +0545

    vendor route updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone')->unique();
             $table->string('password');
-            $table->string('address');
-            $table->string('location');
+            $table->string('address')->nullable();
+            $table->string('location')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
